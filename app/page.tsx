398965cc21b07338da729f26c1b1c7904025fcc3
jun <dri2104@gepplus.co.kr>
2023-10-23 17:34:22 +0900

    Feat: main bg image / menu text / copyright ...

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,20 @@ import SliderBox from "@/components/home/sliderBox";
 const Home: NextPage = () => {
   return (
     <Layout title="홈">
-      <div className="absolute top-0 h-full w-full bg-[url('/assets/main_bg_b.jpeg')] bg-cover bg-center">
-        {/* <div className="fixed z-10 h-screen w-screen bg-black bg-opacity-50"></div> */}
-        <section className=""> </section>
-        <SliderBox />
-        {/* <main className="z-20 mx-auto max-w-7xl px-8 sm:px-16">
-          <section className="pt-6"> Contents </section>
-          
+      <div className="absolute top-0 h-full w-full bg-[url('/assets/bg_main_trim.jpg')] bg-cover bg-center">
+        <div className="fixed z-10 h-screen w-screen bg-black bg-opacity-40"></div>
+        {/* <SliderBox /> */}
+        <main className="relative z-20 flex h-full w-full items-center justify-center">
+          <section className="space-y-8 text-center text-white">
+            <p className="bold text-5xl">다올프라이빗에쿼티는</p>
+            <p className="light text-3xl">
+              2021년 설립 이래 성공적으로 가치를 창출하고 있는 선도적인 사모투자
+              전문회사입니다.
+            </p>
+          </section>
+
           <section></section>
-        </main> */}
+        </main>
       </div>
     </Layout>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,7 @@ export default function Header({ placeholder }: any) {
   });
 
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-2 justify-end bg-black bg-opacity-50 px-3 py-1 shadow-md md:px-6 md:py-4">
+    <header className="sticky top-0 z-50 grid grid-cols-2 justify-end px-3 py-1 md:px-12 md:py-8">
       {/* Left - Logo */}
       <div
         // onClick={() => router.push("/")}
@@ -45,7 +45,7 @@ export default function Header({ placeholder }: any) {
       >
         <Link className="z-50 block" href={"/"}>
           <Image
-            src="/assets/daolpe_logo.png"
+            src="/assets/logo_white_kor.png"
             alt="다올 프라이빗에쿼티"
             fill
             className="object-contain object-left py-1"
@@ -69,28 +69,36 @@ export default function Header({ placeholder }: any) {
             isOpen ? "right-0" : "-right-[280px]"
           }  top-0 z-40 h-full w-[280px] bg-white bg-opacity-90 transition-all ease-in`}
         >
-          <ul className="bolder m-auto space-y-8 px-10 pt-32 text-2xl text-[#333]">
-            <li>
-              <Link className="block py-2" href={"/"}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="block py-2" href={"/company"}>
-                Company
-              </Link>
-            </li>
-            <li>
-              <Link className="block py-2" href={"/portfolio"}>
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link className="block py-2" href={"/leadership"}>
-                Leadership
-              </Link>
-            </li>
-          </ul>
+          <div className="grid h-full grid-cols-1 grid-rows-2 items-center justify-end">
+            <ul className="bolder space-y-8 pt-32 text-center text-2xl text-[#222222]">
+              <li>
+                <Link className="block py-2" href={"/"}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="block py-2" href={"/company"}>
+                  Company
+                </Link>
+              </li>
+              <li>
+                <Link className="block py-2" href={"/portfolio"}>
+                  Portfolio
+                </Link>
+              </li>
+              <li>
+                <Link className="block py-2" href={"/leadership"}>
+                  Leadership
+                </Link>
+              </li>
+            </ul>
+            <div className="flex h-full flex-col flex-wrap items-center justify-end pb-4">
+              <span className="px-4 text-center text-sm">
+                COPYRIGHT© <br />
+                DAOL Private Equity ALL RIGHTS RESERVED.
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </header>
