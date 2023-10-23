12b1f9e03b3c057389262498c4c46ce9a9178347
jun <dri2104@gepplus.co.kr>
2023-10-23 15:03:36 +0900

    Feat: Header - menu slide state - number -> string 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,24 +7,24 @@ import Hamburger from "hamburger-react";
 
 export default function Header({ placeholder }: any) {
   const [isOpen, setOpen] = useState(false);
-  const [xPosition, setX] = useState(-280);
+  const [xPosition, setX] = useState("280");
   const side = useRef(null);
   // const router = useRouter();
 
   const toggleMenu = () => {
-    if (xPosition < 0) {
+    if (+xPosition > 0) {
       setOpen(true);
-      setX(0);
+      setX("0");
     } else {
       setOpen(false);
-      setX(-280);
+      setX("280");
     }
   };
 
   const handleClose = async () => {
     let sideArea = side.current;
     if (isOpen && !sideArea) {
-      await setX(-280);
+      await setX("280");
       await setOpen(false);
     }
   };
@@ -37,13 +37,13 @@ export default function Header({ placeholder }: any) {
   });
 
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 justify-end bg-black bg-opacity-50 px-3 py-1 shadow-md md:px-6 md:py-4">
+    <header className="sticky top-0 z-50 grid grid-cols-2 justify-end bg-black bg-opacity-50 px-3 py-1 shadow-md md:px-6 md:py-4">
       {/* Left - Logo */}
       <div
         // onClick={() => router.push("/")}
-        className="relative my-auto flex h-10 w-16 cursor-pointer items-center md:w-auto "
+        className="relative my-auto flex h-10 w-full cursor-pointer items-center"
       >
-        <Link className="z-50" href={"/"}>
+        <Link className="z-50 block" href={"/"}>
           <Image
             src="/assets/daolpe_logo.png"
             alt="다올 프라이빗에쿼티"
@@ -52,8 +52,6 @@ export default function Header({ placeholder }: any) {
           />
         </Link>
       </div>
-
-      <div />
 
       {/* Right */}
       <div className="flex items-center justify-end space-x-14 text-lg text-gray-500">
@@ -67,7 +65,7 @@ export default function Header({ placeholder }: any) {
 
         <div
           ref={side}
-          className={`fixed right-0 top-0 z-40 h-full w-[280px] transform bg-white bg-opacity-90 translate-x-[${-xPosition}px] transition-all ease-in`}
+          className={`fixed -right-[${xPosition}px] top-0 z-40 h-full w-[280px] bg-white bg-opacity-90  transition-all ease-in`}
         >
           <ul className="bolder m-auto space-y-8 px-10 pt-32 text-2xl text-[#333]">
             <li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      translate: {
+        menuX: "280px",
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
